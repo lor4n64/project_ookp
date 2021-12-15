53128b9456cc3d9da42e899a0d234a92dc79900a
lor4n64 <kilowwatilya@gmail.com>
2021-12-15 06:16:58 +0200

    database

---
+++ contacts_db/Student.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT (newid()), 
+    [FirstName] NCHAR(10) NULL, 
+    [LastName] NCHAR(50) NULL, 
+    [Patronym] NCHAR(50) NULL, 
+    [GroupNum] INT NULL, 
+    [SpecialityCode] INT NULL, 
+    [RecordBookNum] INT NULL
+)
